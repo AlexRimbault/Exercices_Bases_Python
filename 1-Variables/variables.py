0eee78b5bc7aff0f69b2b6f4d801256ea32b9d4e
AlexandreRimbault <alexandrerimbault7@gmail.com>
2019-05-22 19:45:28 +0200

    Ajout du premier dossier contenant le fichier sur la partie 1 des exos

---
+++ 1-Variables/variables.py
@@ -0,0 +1,98 @@
+#BASES
+
+#1
+
+développeur = "Python"
+print(développeur)
+
+#2
+#TODO
+age = 25
+prénom = "Alexandre"
+phrase = "Je suis {prénom} et j'ai {age} ans"
+print(phrase + prénom + age)
+#TODO
+
+#3
+#TODO
+
+#4
+nombre = 3
+print(nombre*4)
+
+#5
+nombre = 1
+addition = nombre + 1
+print(addition)
+
+#nombre = 1
+print(nombre+1)
+
+#6
+nombre = 1
+soustraction = nombre - 1
+print(soustraction)
+
+#nombre = 1
+print(1-1)
+
+#7
+nombre = 1
+multiplication = nombre * 2
+print(multiplication)
+
+nombre = 1
+print(nombre*2)
+
+#8
+nombre = 1
+division = nombre / 2
+print(division)
+
+#nombre = 1
+print(nombre/2)
+
+#9
+a = 5
+b = 7
+a, b = b, a
+print (a,b)
+
+#10
+#TODO
+
+#11
+#TODO
+
+#12
+#TODO
+
+#LES LISTES
+
+#13
+#TODO
+
+#14
+#TODO
+
+#15
+#TODO
+
+#16
+#TODO
+
+#17
+#TODO
+
+#18
+#TODO
+
+#LES DICTIONNAIRES
+
+#19
+#TODO
+
+#LISTES, DICTIONNAIRES, TUPLES
+
+#20
+#TODO
