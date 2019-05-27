9f9729a030ad8c14a2d8fd8f2e61877a40af3d79
AlexandreRimbault <alexandrerimbault7@gmail.com>
2019-05-28 00:35:50 +0200

    Ajout du troisieme dossier contenant le fichier sur la partie 3 des exos

---
+++ 3-Boucles/boucles.py
@@ -0,0 +1,68 @@
+#boucle for
+
+#1
+
+nombres = 0,1,2,3,4,5
+for nombre in nombres:
+    print(nombre)
+
+#2
+
+liste = ["I", "am", "legend"]
+for mot in liste:
+    print(mot, len(mot))
+
+#3
+
+x = "anticonstitutionnellement"
+for lettre in x:
+    print(lettre)
+
+#4
+#TODO
+x = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+
+
+#5
+#TODO
+
+x = [1,10,20,30,40,50]
+
+#6
+
+for i in reversed(range(6)):
+    print(i)
+
+#7
+
+for i in range(1,10):
+    if i > 3:
+        break
+        print(i)
+
+#8
+
+nombre = 1,2,3,4,5,6,7,8,9,10
+for nombre in range(1,10):
+    if nombre > 3:
+        break
+        print(nombre)
+
+#9
+
+for i in range (1,10):
+    if i == 3:
+        continue
+        print(i)
+
+#boucle while
+
+#10
+#TODO
+
+#11
+#TODO
+
+#12
+#TODO
