e24d3ec5e1a24a19488d1533e155233051d3677d
AlexandreRimbault <alexandrerimbault7@gmail.com>
2019-06-07 22:31:08 +0200

    Fichier sur fonctions finalisé

--- 4-Fonctions/fonctions.py
+++ 4-Fonctions/fonctions.py
@@ -7,10 +7,16 @@ nombre(7)
 
 #2
 
-def nombres_pairs(liste = [1,2,3,4,5,6,7,8,9,10]):
-    return(liste[1],liste[3],liste[5],liste[7])
+def nbp(liste_nb):
+    liste_pairs = []
+    for element in liste_nb:
+        if element % 2 == 0:
+            liste_pairs.append(element)
 
-nombres_pairs()
+    return liste_pairs
+
+a = nbp([1,2,3,4,5,6,7,8,9,10])
+print(a)
 
 #3
 
@@ -27,32 +33,86 @@ fib(100)
 
 #1
 
-def voyelle():
-    chaine = "Hello World"
-    for lettre in chaine:
-        if lettre in "aeiouy":
-            print(lettre)
+voy = ["a", "e", "i", "o", "u", "y"]
+def nbv(caracters):
+    cpt = 0
+    for c in caracters:
+         if c in voy:
+             cpt += 1
 
-voyelle()
+    return cpt
+
+nbv("salut la compagnie") # => 7
 
 #2
-#TODO
+voy = ["a", "e", "i", "o", "u", "y"]
+def nbv(caracters):
+     cpt = 0
+     enum = 0
+     while enum < len(caracters):
+         if caracters[enum] in voy:
+             cpt += 1
+         enum += 1
+
+     return cpt
 
 #3
-#TODO
+def nbv(caracters):
+ cpt = 0
+ for c in caracters:
+if c in "aeiouy":
+cpt += 1
+return cpt
 
 #5
-#TODO
+def facto(nb):
+cpt = nb
+
+while nb != 1:
+nb -= 1
+cpt *= nb
+
+return cpt
+
+4*3*2*1
     
 
 #6
-#TODO
+def factorielle(n):
+	if n == 1 :
+		return 1
+	else:
+		return n * factorielle(n-1)
+
+x = factorielle(5)
+print(x)
 
 #7
-#TODO
+def ma_fonction(*args):
+    len(args)
+    sum(args)
+
+
+ma_fonction(1, 2, 3, 4, 5, 6, 7, 8, 9, 1345679)
 
 #8
-#TODO
+def premier_chiffre(x):
+	x=abs(x)
+	s=str(x)
+	return s[0]
+
+def frequence_premiers_chiffres(liste):
+	d = {c:0 for c in range(1, 10)}
+     d = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+	for x in liste:
+		c=premier_chiffre(x)
+		if c!= 0:
+			d[premier_chiffre(x)] += 1
+	return d
+
+l = [1, 4 ,9 ,16 ,25 ,36 ,49 ,64 ,100 ,121]
+r = frequence_premiers_chiffres(l)
+print(r)
 
 
 
